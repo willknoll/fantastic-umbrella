f46f084d0ab6a42660e01afc9328bb10d2892076
Will Knoll <v-wiknol@microsoft.com>
2020-05-06 13:58:53 -0700

    put backticks back

--- dist/index.js
+++ dist/index.js
@@ -1390,13 +1390,13 @@ const github = __webpack_require__(30);
 try {
 
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log('Hello ${nameToGreet}!');
+  const nameToGreet = core.getInput(`who-to-greet`);
+  console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log('The event payload: ${payload}');
+  console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
